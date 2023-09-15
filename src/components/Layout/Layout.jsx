e4cf9e42c25f52b9767b617f2c880b6dd96538ac
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-09-15 22:31:59 +0300

    2.2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { LayoutW, ListNav, Header, StyledLink, Item } from './Layout styled';
 
 export const Layout = () => {
